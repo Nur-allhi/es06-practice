e149e3de497ce6e77254aa932e54a0e2caa9070e
Nur-allhi <noorefty1@gmail.com>
2021-04-15 13:14:13 +0600

    learning1

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function sum(num1, num2) {
-  num2 = num2 || 5;
-  return num1 + num2;
-}
+// function sum(num1, num2 = 10) {
+//   //   num2 = num2 || 5;
+//   return num1 + num2;
+// }
 
-const total = sum(15, 2);
-console.log(total);
+// const total = sum(15);
+// console.log(total);
+
+// const firstName = "Justin";
+// const lastName = "Bibber";
+// const fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+const doubleIt = (num) => num * 2;
+const add = (x, y) => x + y;
+const result = add(50, 20);
+const result2 = doubleIt(10);
+console.log(result);
---
+++ threedots.js
@@ -0,0 +1,10 @@
+// const ages = [10, 9, 13, 16];
+// const ages1 = [20, 24, 25, 26, 28, 29];
+// const ages2 = [34, 35, 32, 38];
+
+// const allAges = [...ages, ...ages2, 6, ...ages2];
+// console.log(allAges);
+
+const takaPoisha = [650, 300, 100, 500, 700, 900];
+const maximum = Math.max(...takaPoisha);
+console.log(maximum);
